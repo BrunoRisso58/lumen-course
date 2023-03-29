eaebf3f4376a9f48644bc050be7208275d5244df
Bruno Risso <brunorisso58@gmail.com>
2023-03-29 16:22:28 -0300

    Building model, factory, controller (body), migration, seeder, web routes and traits

---
+++ LumenAuthorsApi/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Traits\ApiResponser;
+
+class AuthorController extends Controller
+{
+    use ApiResponser;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Return the list of authors
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Create one new author
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Obtain and show one author
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function show($author)
+    {
+
+    }
+
+    /**
+     * Update an existing author
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $author)
+    {
+
+    }
+
+    /**
+     * Remove an existing author
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function destroy($author)
+    {
+
+    }
+}
