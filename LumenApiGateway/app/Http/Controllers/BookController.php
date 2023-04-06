fdd13d5e36de7feb750ff57199b30974a725cf82
Bruno Risso <brunorisso58@gmail.com>
2023-04-06 15:15:37 -0300

    Creating Api Gateway structure

---
+++ LumenApiGateway/app/Http/Controllers/BookController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Traits\ApiResponser;
+use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class BookController extends Controller
+{
+    use ApiResponser;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Return the list of books
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Create one new book
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+    }
+
+    /**
+     * Obtain and show one book
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function show($book)
+    {
+        
+    }
+
+    /**
+     * Update an existing book
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $book)
+    {
+        
+    }
+
+    /**
+     * Remove an existing book
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function destroy($book)
+    {
+        
+    }
+}
